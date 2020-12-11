90754cbb4aa2b0c16507c825ecd7b9a6bca3ad55
CharlesFahrenwald <75191202+CharlesFahrenwald@users.noreply.github.com>
2020-12-11 11:50:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 $(function(){
-    /*$('ul').on('click','li', function(event){
-        alert("you clicked a list");
-
-        /*$('li').on('click', 'shopping-item-delete', function(event){
-            //this.closest('li').remove();
-        });
-    });*/
-    $('li').on('click', '.shopping-item-delete', function(event){
+    $('shopping-list').on('click', '.shopping-item-delete', function(event){
         //alert("Clicked on li")
         //alert("you clicked the delete button");
-        this.closest('li').remove();
+        $(this).closest('li').remove();
     });
-    $('li').on('click', '.shopping-item-toggle', function(event){
-        $(this).toggleClass("shopping-item shopping-item__checked")
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        $(this).closest('li').find('.shoping-item').toggleClass("shopping-item__checked")
     });
 
-        $("form").submit(event => {
+        $("#js-shopping-list-form").submit(event => {
             //capturing input
         event.preventDefault();
-        const userTextElement = $(event.currentTarget).find("#shopping-list-entry");
+        const listItem = $('.js-shopping-list-entry').val();
         //creating elements that we want to appear on screen
-        var Name;
-        var ul = document.getElementsByClassName("shopping-list");
+        
+        var ul = document.createElement("shopping-list");
         var item_li = document.createElement("li");
         // step 1) Create an element (e.g. div, li, span, button)
         var spanToAdd = document.createElement("span");
@@ -30,77 +23,33 @@ $(function(){
         var button_delete = document.createElement("button");
         var spanButCheck = document.createElement("span");
         var spanButDel = document.createElement("span");    
-
+        var form = document.createElement("form")    
         spanToAdd.classList.add("shopping-item");
         spanToAdd.classList.add("shopping-item shopping-item__checked");
         spanToAdd.textContent = userTextElement.val();
 
         divToAdd.classList.add("shopping-item-controls");
 
-        button_check.setAttribute('className', "shopping-item-toggle");
-        button_delete.setAttribute('className', "shopping-item-delete");
+        button_check.add("shopping-item-toggle");
+        button_delete.add("shopping-item-delete");
 
-        spanButCheck.setAttribute('className', "button-label");
+        spanButCheck.add("button-label");
         spanButCheck.textContent = "check"
 
-        spanButDel.setAttribute('className', "button-label");
+        spanButDel.add("button-label");
         spanButDel.textContent = "delete"
 
-        button_check.appendChild(spanButCheck);    
-        button_delete.appendChild(spanButDel);    
+        button_check.add(spanButCheck);    
+        button_delete.add(spanButDel);    
 
-        divToAdd.appendChild(button_check);
-        divToAdd.appendChild(button_delete);    
+        divToAdd.add(button_check);
+        divToAdd.add(button_delete);    
 
-        item_li.appendChild(spanToAdd);
-        item_li.appendChild(divToAdd);
+        item_li.add(spanToAdd);
+        item_li.add(divToAdd);
 
-        ul[0].appendChild(item_li);
-        // Step 2) adjusting the elements' fields (e.g. class, textContent)
-
-        // step 3) attatch the elements to the appropriate parent element
-
-    })
-        //alert("you pressed delete")
-        /*$('shopping-list shopping-item').live('click',function(event){
-            $(this).parent().remove();
-        });*/
-});
-/*$("form").submit(event => {
-    event.preventDefault();
-    const userTextElement = $(event.currentTarget).find("shopping-list-entry");
-    $(".js-display-user-text").text(`user text is:  ${userTextElement.val()}`);
-    userTextElement.val("");
-    $('ul').add("button")
-  });*/
-
-
-
-
-
-
-
-
-
-  /*$("form").submit(event => {
-    //capturing input
-    event.preventDefault();
-    const userTextElement = $(event.currentTarget).find("#shopping-list-entry");
-    //creating elements that we want to appear on screen
-    var li = document.createElement("li");
-    var div = document.createElement("div");
-    var span = document.createElement("span");
-    var button_delete = document.createElement("button");
-    var button_check = document.createElement("button");
-    //span.addClass("shopping-item");
-    // Setting the span's text equal to the user input (what they want to add to the list)
-    span.textContent = userTextElement.val();
-    // Attatching the button elements that we created to the div
-    div.append(button_check);
-    div.append(button_delete);
-    // Attatching the span to the list
-    li.append(span);
-    // Attatching the div to the list
-    li.append(div);
-    // Attatching the li to ul
-    $("ul").append(li);*/
+        ul.add(item_li);
+        
+        form.add(ul);
+        });
+    });
